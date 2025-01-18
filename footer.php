031bf6fcda4d383a7d3ae75e11d1ac0e317a9729
SmrutiSikha08 <SikhaMahapatra2004@gmail.com>
2025-01-18 19:01:17 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer>
+    <div class="container">
+      <div class="col-lg-8">
+        <p>@TastyYummies All Rights Reserved.A Raptive Partner Site.
+         <br> Learn More || Privacy Policy</p>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/counter.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+  </body>
+</html>
